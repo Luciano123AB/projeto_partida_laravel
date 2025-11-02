a0fb7e6ba034e02164d5ff3ee770e4fd6d8d15b7
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2025-11-02 17:58:35 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MainController;
 use App\Http\Middleware\Checar;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", [MainController::class, "home"])->name("Home");
+Route::get("/", [MainController::class, "home"])->name("home");
 
 Route::middleware([Checar::class])->group(function() {
     Route::prefix("/")->group(function () {
