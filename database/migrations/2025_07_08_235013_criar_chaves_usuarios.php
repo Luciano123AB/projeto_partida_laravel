e44ffe6ac553733baae8794c4b239d8affa80fdc
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2025-11-17 17:52:09 -0300

    Update 2025_07_08_235013_criar_chaves_usuarios.php

--- database/migrations/2025_07_08_235013_criar_chaves_usuarios.php
+++ database/migrations/2025_07_08_235013_criar_chaves_usuarios.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table("players", function (Blueprint $table) {
-            $table->unsignedBigInteger("id_notas")->nullable()->after("id");
-            $table->foreign("id_notas")->references("id")->on("personagens")->comment("1|2|3");
+            $table->unsignedBigInteger("id_nota")->nullable()->after("id");
+            $table->foreign("id_nota")->references("id")->on("personagens")->comment("1|2|3");
         });
     }
 
