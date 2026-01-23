97d90a0698e475cac4ee29e33b69a6e170e07976
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2026-01-23 01:40:01 -0300

    Reorder use statements in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model implements MustVerifyEmail
