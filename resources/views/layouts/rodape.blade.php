e4666b17b31b816e2554c47ed54e0cc51d1b1ca7
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2026-04-02 10:44:12 -0300

    Update rodape.blade.php

--- resources/views/layouts/rodape.blade.php
+++ resources/views/layouts/rodape.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-dark text-light py-3">
+<footer class="bg-dark text-light mt-auto py-3">
     <img style="width: 35px; height: 35px;" class="border" src="{{ asset("assets/images/perfil.png") }}">
     Todos os Direitos Reservados: Luciano Eduardo Stefanello da Silva
     <br>
