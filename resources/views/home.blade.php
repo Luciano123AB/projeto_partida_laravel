671a7f265db3c7c27e0ece3c05dc4d69bfa50966
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2026-04-14 19:52:11 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,11 @@
 @extends("layouts.main_layout")
 
 @section("content")
+    @can("create", App\Models\Nota::class)
+    @endcan
+
     <x-componente :user="Auth::user()" />
+
+    @can("user_is_admin")
+    @endcan
 @endsection
