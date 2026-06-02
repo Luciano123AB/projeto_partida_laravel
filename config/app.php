fe8e43c4c1b96de78628f00200cc6f4a634b56d6
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2026-06-02 10:39:14 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://projeto_partida_laravel.com.br'),
+    'url' => env('APP_URL', 'http://projeto_partida_laravel.test'),
 
     /*
     |--------------------------------------------------------------------------
