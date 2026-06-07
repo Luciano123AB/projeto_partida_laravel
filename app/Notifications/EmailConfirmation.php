8fd32ec9c78104eb16f413bdb0069261b43281af
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2026-06-07 15:22:29 -0300

    Update EmailConfirmation.php

--- app/Notifications/EmailConfirmation.php
+++ app/Notifications/EmailConfirmation.php
@@ -3,16 +3,13 @@
 namespace App\Notifications;
 
 use Carbon\Carbon;
-use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\URL;
 
-class EmailConfirmation extends Notification
+class EmailConfirmation extends Notification implements ShouldQueue
 {
-    use Queueable;
-
     /**
      * Create a new notification instance.
      */
