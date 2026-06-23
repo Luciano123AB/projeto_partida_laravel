de6066f5311e4e0d93e71efbdf108c3ecc6d3e50
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-23 18:36:06 -0300

    master

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,5 +2,5 @@
     <img style="width: 35px; height: 35px;" class="border rounded" src="{{ asset("assets/images/perfil.png") }}">
     Todos os Direitos Reservados: Luciano Eduardo Stefanello da Silva
     <br>
-    © 2026 - {{ date("Y") }} {{ env("APP_NAME") }}
+    © 2026 - {{ date("Y") }} {{ config('app.name') }}
 </footer>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar bg-dark">
     <div class="container-fluid">
-        <a href="{{ route("index") }}" class="navbar-brand text-decoration-none text-light fs-2"><i class="bi bi-card-image fs-1"></i> {{ env("APP_NAME") }}</a>
+        <a href="{{ route("index") }}" class="navbar-brand text-decoration-none text-light fs-2"><i class="bi bi-card-image fs-1"></i> {{ config('app.name') }}</a>
 
         <div>
             @guest
